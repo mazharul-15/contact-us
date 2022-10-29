7e14cbeab98f9895c56ff609834e7980ae3a8317
mazharul-15 <mazharulislamhstu15@gmail.com>
2022-10-29 23:05:59 +0600

    4th commit

---
+++ admin/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/admin-login.css">
+    <title>Contact Us Admin</title>
+</head>
+<body>
+    <div class="main">
+        <form action="#" method = "POST">
+
+            <!-- Email -->
+            <div class="input-field">
+                <label for="email">Email:</label>
+                <input type="email" name="email" class = "input-all" id="email" placeholder = "enter your email">
+            </div>
+
+            <!-- Password -->
+            <div class="input-field">
+                <label for="password">Password:</label>
+                <input type="password" name="password" class = "input-all" id="password" placeholder = "enter your password">
+            </div>
+
+            <!-- submit -->
+            <div class="input-field">
+                <input type="submit" class = "input-all" value="Log In">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Contact Us</title>
+</head>
+<body>
+    <div class="main">
+        <div class="col-3">
+            <div class="container">
+                <i class="fa-solid fa-envelope icons-evlop"></i>
+                <h2>Contact Us</h2>
+                <h4>We would love to hear frome you !</h4>
+            </div>            
+        </div>
+        <div class="col-7">
+            <div class="container">
+                <form action="#" method = "POST">
+
+                        <!-- Name Field -->
+                        <div class="input-field">
+                            <label for="name">Name:</label>
+                            <input type="text" name="name" class = "input-all" id="name" placeholder="enter your name">
+                        </div>
+        
+                        <!-- Email Field -->
+                        <div class="input-field">
+                            <label for="email">Email:</label>
+                            <input type="email" name="email" class = "input-all" id="email" placeholder = "enter your email">
+                        </div>
+        
+                        <!-- Mobile Field -->
+                        <div class="input-field">
+                            <label for="mobile">Mobile:</label>
+                            <input type="text" name="mobile" class = "input-all" id="mobile" placeholde = "enter your mobile">
+                        </div>
+        
+                        <!-- Comment Field -->
+                        <div class="input-field">
+                            <label for="comment">Comment:</label>
+                            <textarea name="comment" class = "input-all" id="comment" cols="30" rows="10" placeholder = "write your comment"></textarea>
+                        </div>
+        
+                        <!-- Submit Field -->
+                        <div class="input-field">
+                            <input type="submit" value="Submit">
+                        </div>
+                    </div>
+                </form>
+        </div>
+    </div>
+
+    <script src="javascript/script.js"></script>
+</body>
+</html>
